34753b95657db053226b9146c68218313e3979ce
aznkillahx <Jason.JY.Liu@gmail.com>
2015-09-25 13:09:04 -0400

    TDD: made simple player ID check

---
+++ src/Assignment1/GameTest.java
@@ -0,0 +1,39 @@
+package Assignment1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GameTest {
+
+	@Test
+	public void test() {
+		
+		//Manual inputs as the origin class does not allow inputs less than 2 and greater than 4 for 
+		//number of players in the game
+		Game G1 = new Game(2);
+		Game G2 = new Game(3);
+		Game G3 = new Game(4);
+		
+		Player P1 = new Player("Player1");
+		Player P2 = new Player("Player2");
+		Player P3 = new Player("Player3");
+		Player P4 = new Player("Player4");
+		
+		//Testing player creation with 2 players
+		assertEquals(G1.getPlayerList().get(0).getID(), P1.getID());
+		assertEquals(G1.getPlayerList().get(1).getID(), P2.getID());	
+
+		//Testing player creation with 3 players
+		assertEquals(G2.getPlayerList().get(0).getID(), P1.getID());
+		assertEquals(G2.getPlayerList().get(1).getID(), P2.getID());
+		assertEquals(G2.getPlayerList().get(2).getID(), P3.getID());
+		
+		//Testing player creation with 4 players
+		assertEquals(G3.getPlayerList().get(0).getID(), P1.getID());
+		assertEquals(G3.getPlayerList().get(1).getID(), P2.getID());
+		assertEquals(G3.getPlayerList().get(2).getID(), P3.getID());
+		assertEquals(G3.getPlayerList().get(3).getID(), P4.getID());
+	}
+
+}
